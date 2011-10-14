ec756c5bc495bc4cb7e7568be3be9908f0ced03c
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-14 16:58:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DebateApp::Application.config.secret_token = '8c93e0ae750ff6cd051904d355ef1228a1a9146586679c5828a72b8b1c6536dbcb5fd8c95c0ed06865681c4cd1c1c7f36fd2291ead5bf16af5ffa2b9aa8236f7'
