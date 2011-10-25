587fd157979f931765c509a01d9a51f58b3f0d48
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-25 18:14:36 -0400

    ran migration

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20111025211722) do
+
+  create_table "arguments", :force => true do |t|
+    t.integer  "debater_id"
+    t.integer  "debate_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "debater_debates", :force => true do |t|
+    t.integer  "debater_id"
+    t.integer  "debate_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "debaters", :force => true do |t|
+    t.string   "name"
+    t.string   "encrypted_password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "debates", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
