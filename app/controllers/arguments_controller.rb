adde3079d425de00e7e6590da3994f9ca0a224cc
Deepal Chadha <deepal.chadha@gmail.com>
2011-10-31 15:40:59 -0400

    set up looping ajax requests that works

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -1,7 +1,6 @@
 class ArgumentsController < ApplicationController
 
-  def create
-    	
+  def create    	
 	@debate_id = params[:argument][:debate_id]
     @debate = Debate.find_by_id(@debate_id)
 	@timeleft = time_left(@debate)
@@ -14,8 +13,11 @@ class ArgumentsController < ApplicationController
 	else
 		# redirect without creating argument
 		redirect_to debate_path(params[:argument][:debate_id])
-	end	
-	
+	end		
+  end
+  
+  def index
+    # @comments = Comment.where("article_id = ? and created_at > ?", params[:article_id], Time.at(params[:after].to_i + 1))
   end
   
 end
