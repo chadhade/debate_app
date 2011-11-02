d91c1aa1a948505fa3a7eba8fa38be901349e563
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-02 11:22:20 -0400

    creates new votes object once clicked

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,9 @@
 class VotesController < ApplicationController
   def create
-    # current_debater.vote(@argument, true or false)
+	@argument = Argument.find_by_id(params[:vote][:votable_id])
+	@vote = params[:vote][:vote]
+	current_debater.vote(@argument, :direction => :up) if @vote == "true"
+	current_debater.vote(@argument, :direction => :down) if @vote == "false"
 	respond_to do |format|
 	  format.html
 	  format.js
