9418229e553e38e7fe343ade1df507bd68bcc56e
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-04 17:42:02 -0400

    a registered debater can track debates but haven't set up the tracking index page

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -6,6 +6,12 @@ class TrackingsController < ApplicationController
   end
 
   def create
+	@debate_id = params[:debate_id]
+	current_debater.trackings.create(:debate_id => @debate_id)
+    respond_to do |format|
+	  format.html
+	  format.js {render :nothing => true}
+	end
   end
 
   def destroy
