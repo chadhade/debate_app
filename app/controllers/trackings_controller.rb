af8f4927d4a5d1f7a8c9ebad53bf3d510405f444
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-05 10:49:04 -0400

    fixed tracking associations

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,5 +1,12 @@
 class TrackingsController < ApplicationController
   def index
+	# load all debates that this debater is tracking into @tracking_debates
+	@debater = Debater.find(params[:debater_id])
+	@tracking_debates = @debater.tracking_debates
+	
+	# load all arguments for those debates into corresponding array @tracking_debates_arguments
+	@tracking_debates_arguments = Array.new
+	@tracking_debates.each {|debate| @tracking_debates_arguments << debate.arguments}
   end
 
   def new
