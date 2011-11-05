c5d749382cb3729fe75da57b672e1b8ef2600eee
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-05 11:37:25 -0400

    got trackings index page working

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -3,7 +3,6 @@ class TrackingsController < ApplicationController
 	# load all debates that this debater is tracking into @tracking_debates
 	@debater = Debater.find(params[:debater_id])
 	@tracking_debates = @debater.tracking_debates
-	
 	# load all arguments for those debates into corresponding array @tracking_debates_arguments
 	@tracking_debates_arguments = Array.new
 	@tracking_debates.each {|debate| @tracking_debates_arguments << debate.arguments}
