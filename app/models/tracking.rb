f5e8de2d4044e5fa320cec1e4e95a77f37793345
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-09 14:35:07 -0500

    added uniqueness validation for trackings

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -1,4 +1,6 @@
 class Tracking < ActiveRecord::Base
   belongs_to :tracking_debates, :foreign_key => :debate_id
   belongs_to :debater_tracking, :foreign_key => :debater_id
+  
+  validates :debater_id, :presence => true, :uniqueness => {:scope => :debate_id}
 end
