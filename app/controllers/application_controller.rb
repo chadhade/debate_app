9a3b41888ad5fd98877429515c92b42aa7ff7f22
Rick Batra <Batra.Rick@gmail.com>
2011-11-17 12:29:16 -0500

    Altered after_sign_in and after_sign_up properties of Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,11 @@ class ApplicationController < ActionController::Base
 		end
 	end
   
-  end
+	def after_sign_in_path_for(resource)
+		stored_location_for(resource) || debates_path
+	end
+ 
+	def after_sign_up_path_for(resource)
+		stored_location_for(resource) || debates_path
+	end
+ end
