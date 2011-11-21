7b523e63fe02e6e261c294d435289800b8471f1a
Rick Batra <Batra.Rick@gmail.com>
2011-11-20 20:04:35 -0500

    Arguments now being stripped of footnotes correctly

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -19,22 +19,26 @@ class Argument < ActiveRecord::Base
   end
 
   def save_footnote
-	@content = self.content
-	@partialcontent = @content
-	#Find the position of the first footnote
-	@startposition = @partialcontent.index(/\(\((.+)\)\)/)
+	content = self.content
 	
-	until @startposition.nil? do
-		@endposition = @partialcontent.index(/\)\)/, @startposition - 1)
-		#Save the footnote and its position. Then find the position of the next footnote.
-		@footnote = @partialcontent[@startposition + 2..@endposition - 1]
-		footnotes.create!(:content => @footnote, :position => @startposition)
-		@startposition = @partialcontent.index(/\(\((.+)\)\)/, @endposition)
+	#Find the position of the first footnote
+	startposition = content.index(/\(\((.+)\)\)/)
+	endposition = -2
+	content_stripped = ""
+	
+	#Save the footnote and its position. Then find the position of the next footnote.
+	until startposition.nil? do
+		#Produce a version without footnotes
+		content_stripped = content_stripped + content[endposition + 2..startposition - 1]
+		
+		endposition = content.index(/\)\)/, startposition - 1)
+		@footnote = content[startposition + 2..endposition - 1]
+		footnotes.create!(:content => @footnote, :position => startposition)
+		startposition = content.index(/\(\((.+)\)\)/, endposition)
 	end
 	
-	#Remove the footnote from the argument
-	@content = @content.gsub(/\(\((.+)\)\)/,"")
-	self.update_attributes(:content => @content, :any_footnotes => true)
+	self.update_attributes(:content => content_stripped, :any_footnotes => true)
+	
   end
   
   def show_footnote
@@ -43,7 +47,8 @@ class Argument < ActiveRecord::Base
 	self.footnotes.each do |footnote|
 		@footnote_as_link = "<a href=\"#\" title=#{footnote.content} class=\"footnote\"> <span>#{footnote.id}</span> </a>"
 		@content.insert(footnote.position + placeholder, @footnote_as_link)
-		placeholder = placeholder + footnote.content.length
+		placeholder = (placeholder + @footnote_as_link.length) - footnote.content.length - 4
+		#return placeholder
 	end
 	@content.html_safe
   end
