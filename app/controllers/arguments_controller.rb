eb1bb554e855bb16b4e539634f5dde0c9d97a5ea
Deepal Chadha <deepal.chadha@gmail.com>
2011-11-23 12:54:22 -0500

    fixed indenting

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -1,118 +1,103 @@
 class ArgumentsController < ApplicationController
 
   def create    	
-	@debate_id = params[:argument][:debate_id]
-    @debate = Debate.find_by_id(@debate_id)
-	@lastargument = @debate.arguments.last	
-	@timeleft = time_left(@debate)
+  	@debate_id = params[:argument][:debate_id]
+      @debate = Debate.find_by_id(@debate_id)
+  	@lastargument = @debate.arguments.last	
+  	@timeleft = time_left(@debate)
 	
-	# Check if argument is made on time
-	if (@timeleft > 0) && (@debate.current_turn?(current_debater))
-		# create a new argument and redirect to debate page 
-		  # -- Make the repeat_turn column true if it was true before
-		if @lastargument.Repeat_Turn == true 
-			@current_argument = current_debater.arguments.create(:content => params[:argument][:content], :debate_id => params[:argument][:debate_id], 
-										     :time_left => @timeleft, :Repeat_Turn => true)
-		else 
-		  # -- Don't make the repeat_turn column true
-			@current_argument = current_debater.arguments.create(:content => params[:argument][:content], 
-								:debate_id => params[:argument][:debate_id], :time_left => @timeleft) 
-		end
+  	# Check if argument is made on time
+  	if (@timeleft > 0) && (@debate.current_turn?(current_debater))
+  		# create a new argument and redirect to debate page 
+  		  # -- Make the repeat_turn column true if it was true before
+  		if @lastargument.Repeat_Turn == true 
+  			@current_argument = current_debater.arguments.create(:content => params[:argument][:content], :debate_id => params[:argument][:debate_id], 
+  										     :time_left => @timeleft, :Repeat_Turn => true)
+  		else 
+  		  # -- Don't make the repeat_turn column true
+  			@current_argument = current_debater.arguments.create(:content => params[:argument][:content], 
+  								:debate_id => params[:argument][:debate_id], :time_left => @timeleft) 
+  		end
         
-		# Check if there are footnotes attached
-		@current_argument.has_footnote? ? @current_argument.save_footnote(@debate) : nil
-	end
-  
-	
+  		# Check if there are footnotes attached
+  		@current_argument.has_footnote? ? @current_argument.save_footnote(@debate) : nil
+  	end
   end
   
   def index
-	@arguments_params = parse_arguments_params_string(params[:arguments_params])
-	@arguments = new_arguments(@arguments_params)
+  	@arguments_params = parse_arguments_params_string(params[:arguments_params])
+  	@arguments = new_arguments(@arguments_params)
 	
-	# @debate = Debate.find_by_id(params[:debate_id])
-	# @debateid = @debate.id
-	@currentdebater = current_debater
+  	# @debate = Debate.find_by_id(params[:debate_id])
+  	# @debateid = @debate.id
+  	@currentdebater = current_debater
 	
-	@voting_params = parse_voting_params_string(params[:voting_params])
-	@votes = new_votes(@voting_params)
+  	@voting_params = parse_voting_params_string(params[:voting_params])
+  	@votes = new_votes(@voting_params)
 	
-	@debates = Array.new
-	@arguments_params.each {|debate_entry| @debates << debate_entry[:id]}
-	@currently_viewing_counts = updated_currently_viewing_counts(@debates)
+  	@debates = Array.new
+  	@arguments_params.each {|debate_entry| @debates << debate_entry[:id]}
+  	@currently_viewing_counts = updated_currently_viewing_counts(@debates)
   
-	# Add footnotes if they exist
-	@arguments.each do |debates|
-		debates[:new_arguments].each do |argument|
-			argument.any_footnotes ? argument.content = argument.show_footnote : nil
-		end
-	end
+  	# Add footnotes if they exist
+  	@arguments.each do |debates|
+  		debates[:new_arguments].each do |argument|
+  			argument.any_footnotes ? argument.content = argument.show_footnote : nil
+  		end
+  	end
   end
   
   # create a hash of updated currently viewing counts:: :id => debate id, :currently_viewing_count => count
   def updated_currently_viewing_counts(debates)
     currently_viewing_counts = Array.new
-	@debates.each do |debate_id|
-	  currently_viewing_count = Viewing.where("debate_id = ? and currently_viewing = ?", debate_id, true).count
-	  currently_viewing_counts << {:id => debate_id, :currently_viewing_count => currently_viewing_count}
-	end
-	currently_viewing_counts
+  	@debates.each do |debate_id|
+  	  currently_viewing_count = Viewing.where("debate_id = ? and currently_viewing = ?", debate_id, true).count
+  	  currently_viewing_counts << {:id => debate_id, :currently_viewing_count => currently_viewing_count}
+  	end
+  	currently_viewing_counts
   end
   
   # parse arguments_params_string and put into an array of hashes:: :id => #, :after => last argument time
   def parse_arguments_params_string(params_string)
-	arguments_params = Array.new
-	params_string.split("@").each do |debate_entry|
-	  arguments_params << {:id => debate_entry.split(":")[0], :after => debate_entry.split(":")[1]} 
-	end
-	arguments_params
+  	arguments_params = Array.new
+  	params_string.split("@").each do |debate_entry|
+  	  arguments_params << {:id => debate_entry.split(":")[0], :after => debate_entry.split(":")[1]} 
+  	end
+  	arguments_params
   end
   
   # get array of hashes with new arguments:: :id => #, :new_arguments => array of argument objects
   def new_arguments(arguments_params)
     arguments = Array.new
-	arguments_params.each do |debate_entry|
+  	arguments_params.each do |debate_entry|
       debate_id = debate_entry[:id]
-	  new_arguments = Argument.where("debate_id = ? and created_at > ?", debate_entry[:id], Time.at(debate_entry[:after].to_i + 1))
-	  arguments << {:id => debate_id, :new_arguments => new_arguments} unless new_arguments.empty?
-	end
-	arguments
+  	  new_arguments = Argument.where("debate_id = ? and created_at > ?", debate_entry[:id], Time.at(debate_entry[:after].to_i + 1))
+  	  arguments << {:id => debate_id, :new_arguments => new_arguments} unless new_arguments.empty?
+  	end
+  	arguments
   end
   
   # parse voting_params_string and put into an array of hashes:: :id => 1, :times => {:for_time => x, :against_time => y}
   def parse_voting_params_string(params_string)
-	voting_params = Array.new
-	params_string.split("@").each do |arg_entry|
-	  voting_params << {:id => arg_entry.split(":")[0], :times => {:for_time => arg_entry.split(":")[1], :against_time => arg_entry.split(":")[2]}} 
-	end
-	voting_params
+  	voting_params = Array.new
+  	params_string.split("@").each do |arg_entry|
+  	  voting_params << {:id => arg_entry.split(":")[0], :times => {:for_time => arg_entry.split(":")[1], :against_time => arg_entry.split(":")[2]}} 
+  	end
+  	voting_params
   end
   
   # get array of hashes with new values in @votes:: :id => 1, :updated_counts => {:for => x or nil, :against => y or nil}
   def new_votes(voting_params)
     votes = Array.new
-	voting_params.each do |arg_entry|
-	  argument = Argument.find(arg_entry[:id])
-	  new_for_entries = argument.votes.where("vote = ? and created_at > ?", "t", Time.at(arg_entry[:times][:for_time].to_i + 1))
-	  new_for_count = new_for_entries ? argument.votes_for : nil
-	  new_against_entries = argument.votes.where("vote = ? and created_at > ?", "f", Time.at(arg_entry[:times][:against_time].to_i + 1))
-	  new_against_count = new_against_entries ? argument.votes_against : nil
-	  votes << {:id => arg_entry[:id], :updated_counts => {:for => new_for_count, :against => new_against_count}}
-	end
-	votes
-  end
-  
-  # for long polling, not used right now
-  def check_for_new_args(debate_id, after, increment, timeout_length)
-	sleep(increment)
-	@arguments = Argument.where("debate_id = ? and created_at > ?", debate_id, after)    
-	t = 0
-	t += increment
-	until t > timeout_length || !@arguments.empty?
-	  sleep(increment)
-	  @arguments = Argument.where("debate_id = ? and created_at > ?", debate_id, after)
-	  t += increment
-	end
+  	voting_params.each do |arg_entry|
+  	  argument = Argument.find(arg_entry[:id])
+  	  new_for_entries = argument.votes.where("vote = ? and created_at > ?", "t", Time.at(arg_entry[:times][:for_time].to_i + 1))
+  	  new_for_count = new_for_entries ? argument.votes_for : nil
+  	  new_against_entries = argument.votes.where("vote = ? and created_at > ?", "f", Time.at(arg_entry[:times][:against_time].to_i + 1))
+  	  new_against_count = new_against_entries ? argument.votes_against : nil
+  	  votes << {:id => arg_entry[:id], :updated_counts => {:for => new_for_count, :against => new_against_count}}
+  	end
+  	votes
   end
   
 end
--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -2,91 +2,89 @@ class DebatesController < ApplicationController
     
   def new
     # creating a new debate is the same as creating the first argument
-	@argument = Argument.new
+  	@argument = Argument.new
   end
   
   def create
-	# create a new debate linked to debater
-	@debate = Debate.new
-	@debate.save
-	current_debater.debations.create(:debate_id => @debate.id)
+  	# create a new debate linked to debater
+  	@debate = Debate.new
+  	@debate.save
+  	current_debater.debations.create(:debate_id => @debate.id)
 	
-	# create a new argument object
-	@content_of_post = params[:argument][:content]
+  	# create a new argument object
+  	@content_of_post = params[:argument][:content]
 	
-	#The amount of time Debater 1 has left.  
-	@Seconds_Left_1 = (params[:argument][:time_left]).to_i * 60
+  	#The amount of time Debater 1 has left.  
+  	@Seconds_Left_1 = (params[:argument][:time_left]).to_i * 60
 		
-	@argument = current_debater.arguments.create(:content => @content_of_post, :debate_id => @debate.id, :time_left => @Seconds_Left_1)
+  	@argument = current_debater.arguments.create(:content => @content_of_post, :debate_id => @debate.id, :time_left => @Seconds_Left_1)
 	
-	# Check if there are footnotes attached
-	@argument.has_footnote? ? @argument.save_footnote(@debate) : nil
-	redirect_to @debate
+  	# Check if there are footnotes attached
+  	@argument.has_footnote? ? @argument.save_footnote(@debate) : nil
+  	redirect_to @debate
   end
   
   def join
     @debate = Debate.find(params[:id])
-	# link debater to debate
-	current_debater.debations.create(:debate_id => params[:id])
+  	# link debater to debate
+  	current_debater.debations.create(:debate_id => params[:id])
 	
-	#The amount of time Debater 2 has left.  
-	@Seconds_Left_2 = (params[:argument][:time_left]).to_i * 60
+  	#The amount of time Debater 2 has left.  
+  	@Seconds_Left_2 = (params[:argument][:time_left]).to_i * 60
 	
-	# create a new argument object
-	@content_of_post = params[:argument][:content]
-	@argument = current_debater.arguments.create(:content => @content_of_post, :debate_id => params[:id], :time_left => @Seconds_Left_2)
+  	# create a new argument object
+  	@content_of_post = params[:argument][:content]
+  	@argument = current_debater.arguments.create(:content => @content_of_post, :debate_id => params[:id], :time_left => @Seconds_Left_2)
 	
-	# Check if there are footnotes attached
+  	# Check if there are footnotes attached
 	
-	@argument.has_footnote? ? @argument.save_footnote(@debate) : nil
+  	@argument.has_footnote? ? @argument.save_footnote(@debate) : nil
 	
-	redirect_to @debate
+  	redirect_to @debate
   end 
   
   def show
     # pull all arguments from that debate and pass debate object
-	@debate = Debate.find(params[:id])
-	@arguments = @debate.arguments
-	@argument_last = @arguments.last
-	@previoustimeleft = @argument_last.time_left
-	@currentdebater = current_debater
-	debater_signed_in? ? @currentid = @currentdebater.id : @currentid = nil
+  	@debate = Debate.find(params[:id])
+  	@arguments = @debate.arguments
+  	@argument_last = @arguments.last
+  	@previoustimeleft = @argument_last.time_left
+  	@currentdebater = current_debater
+  	debater_signed_in? ? @currentid = @currentdebater.id : @currentid = nil
 	
-	# for viewings
-	update_viewings(@currentdebater, @debate)
+  	# for viewings
+  	update_viewings(@currentdebater, @debate)
 	
-	# Add footnotes if they exist
-	@arguments.each do |argument|
-		argument.any_footnotes ? argument.content = argument.show_footnote : nil
-	end
+  	# Add footnotes if they exist
+  	@arguments.each do |argument|
+  		argument.any_footnotes ? argument.content = argument.show_footnote : nil
+  	end
 	
-	# Calculate the amount of time left for use in javascript timers
-	# If there is only 1 debater, debater 2 has 0 seconds left
-	if @debate.debaters.size == 1
-		@movingclock = 0
-		@staticclock = @previoustimeleft
-		@movingposition = 2
-		return
-	end
+  	# Calculate the amount of time left for use in javascript timers
+  	# If there is only 1 debater, debater 2 has 0 seconds left
+  	if @debate.debaters.size == 1
+  		@movingclock = 0
+  		@staticclock = @previoustimeleft
+  		@movingposition = 2
+  		return
+  	end
 	
-	@timeleft = time_left(@debate)
-	#If a debater has run out of time, the other debater can continuously post
-	if (@timeleft <=0) && (@argument_last.Repeat_Turn != true)
-		@argument_last.update_attributes(:time_left => @argument_last.time_left + @arguments[-2].time_left, :Repeat_Turn => true)
-		@movingclock = @argument_last.time_left - (Time.now - @arglast.created_at).seconds.to_i
-		@staticclock = 0
-		@movingposition = (@argument_last.debater_id != @debate.creator.id) ? 2 : 1
-		@debate = Debate.find(params[:id]) # Reset the debate variable so the view can properly invoke "current_turn"
-		return
-	end
+  	@timeleft = time_left(@debate)
+  	#If a debater has run out of time, the other debater can continuously post
+  	if (@timeleft <=0) && (@argument_last.Repeat_Turn != true)
+  		@argument_last.update_attributes(:time_left => @argument_last.time_left + @arguments[-2].time_left, :Repeat_Turn => true)
+  		@movingclock = @argument_last.time_left - (Time.now - @arglast.created_at).seconds.to_i
+  		@staticclock = 0
+  		@movingposition = (@argument_last.debater_id != @debate.creator.id) ? 2 : 1
+  		@debate = Debate.find(params[:id]) # Reset the debate variable so the view can properly invoke "current_turn"
+  		return
+  	end
 	
-	#Otherwise, determine the order of debaters
-	@argument_last.Repeat_Turn == true ? @previoustimeleft = 0 : nil
-	@movingclock = @timeleft 
-	@staticclock = @previoustimeleft
-	@debate.current_turn == @debate.creator ? @movingposition = 1 : @movingposition = 2
-	
-	
+  	#Otherwise, determine the order of debaters
+  	@argument_last.Repeat_Turn == true ? @previoustimeleft = 0 : nil
+  	@movingclock = @timeleft 
+  	@staticclock = @previoustimeleft
+  	@debate.current_turn == @debate.creator ? @movingposition = 1 : @movingposition = 2
   end
 
 ##############################################################################  
@@ -119,12 +117,12 @@ class DebatesController < ApplicationController
   
   def index
     # returns debates that match search criterion or all debates if empty string submitted
-	@debates = Debate.search(params[:search])
+  	@debates = Debate.search(params[:search])
 	
-	respond_to do |format|
-	  format.html
-	  format.js
-	end
+  	respond_to do |format|
+  	  format.html
+  	  format.js
+  	end
   end
 
 end
