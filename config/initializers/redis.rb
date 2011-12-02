61db0b3f5599acbd97059d177dec6b9f3dfb2d4a
Rick Batra <Batra.Rick@gmail.com>
2011-12-02 18:46:11 -0500

    More heroku changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Redis.connect(:url => ENV['REDISTOGO_URL'])
+Redis.connect(:url => "redis://redistogo:5439f70de5e485ed6ec83e26cc28edde@stingfish.redistogo.com:9222/")
