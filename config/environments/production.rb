6fa43ee61eff614393f239f273b0996144548145
Rick Batra <Batra.Rick@gmail.com>
2011-12-02 19:29:47 -0500

    More heroku junk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,5 @@ DebateApp::Application.configure do
   config.active_support.deprecation = :notify
 
   # For RedisToGo in Heroku
-  ENV["REDISTOGO_URL"] = 'redis://redistogo:5439f70de5e485ed6ec83e26cc28edde@stingfish.redistogo.com:9222/'
+  ENV["REDIS_URL"] = 'redis://redistogo:5439f70de5e485ed6ec83e26cc28edde@stingfish.redistogo.com:9222/'
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-#uri = URI.parse(ENV["REDISTOGO_URL"])
-#REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Redis.connect(:url => ENV['REDISTOGO_URL'])
+uri = URI.parse(ENV["REDIS_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Redis.connect(:url => ENV['REDIS_URL'])
