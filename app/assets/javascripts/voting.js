88d8653f5027a07ecac651947b3c1d718188b661
Rick Batra <Batra.Rick@gmail.com>
2011-12-14 00:55:20 -0500

    Now can't vote until debate has started

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,10 +1,13 @@
+voteable = new Array();
+
 function upVote(argument_id, vote, debate_id) {
   // incrementVotesFor(argument_id);
-  	alert("hi");
-$.ajax({
-      type: "POST",
-      url: "/votes.js?argument_id=" + argument_id + "&vote=" + vote + "&debate_id=" + debate_id
-    });
+	if (voteable[debate_id] == true) {
+		$.ajax({
+	      	type: "POST",
+	      	url: "/votes.js?argument_id=" + argument_id + "&vote=" + vote + "&debate_id=" + debate_id
+	    });
+	}
 }
 function incrementVotesFor(argument_id) {
   $(".votes_for_count[data-argument_id="+argument_id+"]").text(parseInt($(".votes_for_count[data-argument_id="+argument_id+"]").text())+1);
@@ -12,10 +15,12 @@ function incrementVotesFor(argument_id) {
 
 function downVote(argument_id, vote, debate_id) {
   // incrementVotesAgainst(argument_id);
-  	$.ajax({
-      type: "POST",
-      url: "/votes.js?argument_id=" + argument_id + "&vote=" + vote + "&debate_id=" + debate_id
-    });
+  	if (voteable[debate_id] == true) {
+		$.ajax({
+	      	type: "POST",
+	      	url: "/votes.js?argument_id=" + argument_id + "&vote=" + vote + "&debate_id=" + debate_id
+	    });
+	}
 }
 function incrementVotesAgainst(argument_id) {
   $(".votes_against_count[data-argument_id="+argument_id+"]").text(parseInt($(".votes_against_count[data-argument_id="+argument_id+"]").text())+1);
