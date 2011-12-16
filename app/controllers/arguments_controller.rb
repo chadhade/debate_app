7d23c93bcc34477a8f520e6a6b0c8580233ae7aa
Deepal Chadha <deepal.chadha@gmail.com>
2011-12-15 21:23:12 -0500

    commented out if for heroku testing

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -7,7 +7,7 @@ class ArgumentsController < ApplicationController
   	@timeleft = time_left(@debate)
 	
   	# Check if argument is made on time
-  	if (@timeleft > 0) && (@debate.current_turn?(current_debater))
+    # if (@timeleft > 0) && (@debate.current_turn?(current_debater))
   		# create a new argument and redirect to debate page 
   		# -- Make the repeat_turn column true if it was true before
   		if @lastargument.Repeat_Turn == true 
@@ -38,7 +38,7 @@ class ArgumentsController < ApplicationController
   		
   		Juggernaut.publish("debate_" + @debate_id, {:timers => showtimers(@debate, @current_argument, @lastargument), :argument => argument_render, :post_box => post_box_render, :current_turn => @debate.current_turn.email, :footnotes => footnotes_render, :judge => @debate.judge})
   	  reset_invocation_response # allow double rendering
-  	end
+    # end
 
   	respond_to do |format|
   	  format.html
