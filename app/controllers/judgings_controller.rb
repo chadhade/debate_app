e7cbdcd823eafaadb7981d66bc7c57e639cadb65
Deepal Chadha <deepal.chadha@gmail.com>
2011-12-29 15:21:50 -0500

    added judge can't be debater validation

--- app/controllers/judgings_controller.rb
+++ app/controllers/judgings_controller.rb
@@ -7,40 +7,35 @@ class JudgingsController < ApplicationController
 
   def create
     @debate = Debate.find(params[:debate_id])
-    @judge = Judging.new(:debater_id => current_debater.id, :debate_id => @debate.id)
-    @judge.save
-    @debate.update_attributes(:judge => true)
     
-    # If judge joined after both debaters joined, add time spent waiting for judge back to debater 1's time bank
-    # Then start timers
-    if @debate.arguments.count == 2
-      @firstarg = @debate.arguments.first(:order => "created_at ASC")
-      @secondarg = @debate.arguments.all(:order => "created_at ASC").second
-      @oldtime = @firstarg.time_left
-
-      @timeleft = @oldtime + (Time.now - @judge.created_at).seconds.to_i
-      
-      @firstarg.update_attributes(:time_left => @timeleft)
-      
-      @currentturn = @debate.debaters.first(:order => "created_at ASC").email
-      
-      post_box_render = render(:partial => "arguments/form_argument", :locals => {:debate => @debate}, :layout => false)
-  	  reset_invocation_response # allow double rendering
-  	  
-      Juggernaut.publish("debate_" + params[:debate_id], {:func => "judge_arrived", :obj => {:timers => {:movingclock => @oldtime, :staticclock => @secondarg.time_left, :movingposition => 1, 
-                        :debateid => params[:debate_id]}, :argument => "", :judge => true, :post_box => post_box_render, :current_turn => @currentturn}})
+    if !@debate.creator?(current_debater) and !@debate.joiner?(current_debater)
+      @judge = Judging.new(:debater_id => current_debater.id, :debate_id => @debate.id)
+      @judge.save
+      @debate.update_attributes(:judge => true)
+      # If judge joined after both debaters joined, add time spent waiting for judge back to debater 1's time bank
+      # Then start timers
+      if @debate.arguments.count == 2
+        @firstarg = @debate.arguments.first(:order => "created_at ASC")
+        @secondarg = @debate.arguments.all(:order => "created_at ASC").second
+        @oldtime = @firstarg.time_left
+        @timeleft = @oldtime + (Time.now - @judge.created_at).seconds.to_i
+        @firstarg.update_attributes(:time_left => @timeleft)
+        @currentturn = @debate.debaters.first(:order => "created_at ASC").email
+        post_box_render = render(:partial => "arguments/form_argument", :locals => {:debate => @debate}, :layout => false)
+    	  reset_invocation_response # allow double rendering
+        Juggernaut.publish("debate_" + params[:debate_id], {:func => "judge_arrived", :obj => {:timers => {:movingclock => @oldtime, :staticclock => @secondarg.time_left, :movingposition => 1, 
+                          :debateid => params[:debate_id]}, :argument => "", :judge => true, :post_box => post_box_render, :current_turn => @currentturn}})
+      end
+      # remove debate from judging index page
+      Juggernaut.publish("judging_index", {:function => "remove", :debate_id => @debate.id})
+      # update status bar on show page
+      Juggernaut.publish("debate_" + params[:debate_id], {:func => "update_status", :obj => @debate.status})
+      # update individual status
+      Juggernaut.publish("debate_" + @debate.id.to_s, {:func => "update_individual_exists", :obj => {:who_code => "judge", :who_value => "Judge"}})
+      redirect_to @debate
+    else
+      redirect_to :controller => "judgings", :action => "index"
     end
-    
-    # remove debate from judging index page
-    Juggernaut.publish("judging_index", {:function => "remove", :debate_id => @debate.id})
-    
-    # update status bar on show page
-    Juggernaut.publish("debate_" + params[:debate_id], {:func => "update_status", :obj => @debate.status})
-    
-    # update individual status
-    Juggernaut.publish("debate_" + @debate.id.to_s, {:func => "update_individual_exists", :obj => {:who_code => "judge", :who_value => "Judge"}})
-    
-    redirect_to @debate
   end
   
   def submission
