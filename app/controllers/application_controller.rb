077ea2105a3691e46c7c96724604744407949af0
Deepal Chadha <deepal.chadha@gmail.com>
2012-01-03 22:31:51 -0500

    heroku bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
 	def time_left(thisdebate)
 		arguments = thisdebate.arguments(:order => "created_at ASC")
-		arglast = arguments.last
+		arglast = arguments.last(:order => "created_at ASC")
 		
 		if arglast.Repeat_Turn == true
 			timeleft = arglast.time_left - (Time.now - arglast.updated_at).seconds.to_i
@@ -17,10 +17,14 @@ class ApplicationController < ActionController::Base
 		
 		# If there are only 2 arguments, you know the timers first started as soon as the judge joined
 		if arguments.count == 2
-		  return arguments.first.time_left - (Time.now - thisdebate.judge_entry.created_at).seconds.to_i
+		  return arguments.first(:order => "created_at ASC").time_left - (Time.now - thisdebate.judge_entry.created_at).seconds.to_i
 		else
 		# Otherwise, the timers started when the last argument was made
-			timeleft = thisdebate.arguments[-2].time_left - (Time.now - arglast.created_at).seconds.to_i
+      if arguments.count > 2
+			  timeleft = thisdebate.arguments[-2].time_left - (Time.now - arglast.created_at).seconds.to_i
+      else
+		    timeleft = thisdebate.arguments.first(:order => "created_at ASC").time_left - (Time.now - arglast.created_at).seconds.to_i
+	    end
 		end
 	end
   
