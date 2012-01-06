143fd63b3c13cbe61c3ed68fedc2ff0d8b78f33c
Deepal Chadha <deepal.chadha@gmail.com>
2012-01-06 16:12:54 -0500

    fixed topic position debater id issue

--- app/controllers/topic_positions_controller.rb
+++ app/controllers/topic_positions_controller.rb
@@ -2,7 +2,7 @@ class TopicPositionsController < ApplicationController
   before_filter :authenticate_debater!
   
   def create
-    @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => params[:topic_position][:topic], :position => params[:topic_position][:position])
+    @topic_position = TopicPosition.new(:debater_id => current_debater.id, :topic => params[:topic_position][:topic], :position => params[:topic_position][:position])
   	@topic_position.save
   	redirect_to matches_topic_position_path(@topic_position)
   end
