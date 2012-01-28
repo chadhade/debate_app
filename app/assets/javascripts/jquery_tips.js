be3504e3b48908f3827c57b1c5e6d41aec5b134d
Rick Batra <Batra.Rick@gmail.com>
2012-01-27 22:09:55 -0500

    Added instructional javascript tips to debate page

---
+++ app/assets/javascripts/jquery_tips.js
@@ -0,0 +1,59 @@
+// For  Minimize Icon
+$(".waiting-icon").qtip({
+   	content: 'Minimize Debate and Receive Updates.',
+   	position: {
+      corner: {
+         target: 'topMiddle',
+         tooltip: 'bottomMiddle'
+      }
+   	},
+	style: { 
+	 	name: 'blue',
+		color: 'black',
+		border: {
+			width: 2,
+			color: '#0000FF'
+		},
+		width: 275,
+		tip: 'bottomMiddle'
+	}
+});
+$("#info-footnotes").qtip({
+   	content: 'Use double parenthesis like ((this)) to add footnotes to your arguments.',
+   	position: {
+      corner: {
+         target: 'topLeft',
+         tooltip: 'topRight'
+      }
+   	},
+	style: { 
+	 	name: 'blue',
+		color: 'black',
+		border: {
+			width: 2,
+			color: '#0000FF'
+		},
+		width: 250,
+		tip: 'topRight'
+	}
+});
+$("#info-votes").qtip({
+   	content: 'Click on the numbers below to upvote or downvote an argument.',
+   	position: {
+      corner: {
+         target: 'topMiddle',
+         tooltip: 'bottomRight'
+      }
+   	},
+	style: { 
+	 	name: 'blue',
+		color: 'black',
+		border: {
+			width: 2,
+			color: '#0000FF'
+		},
+		width: 250,
+		tip: 'bottomRight'
+	}
+});
+
