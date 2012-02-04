106b77eae4b026db3efb7496870adefb90bb5c4d
Rick Batra <Batra.Rick@gmail.com>
2012-02-04 12:59:28 -0500

    Began reorganizing match page and added ajax functionality

--- app/controllers/topic_positions_controller.rb
+++ app/controllers/topic_positions_controller.rb
@@ -5,6 +5,17 @@ class TopicPositionsController < ApplicationController
     @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => params[:topic_position][:topic], :position => params[:topic_position][:position])
   	@topic_position.save
   	redirect_to matches_topic_position_path(@topic_position)
+    
+    #@matching = Debate.matching_debates(@topic_position)
+	  
+    #respond_to do |format|
+  	  #format.html
+  	  #format.js 
+  	#end
+  end
+  
+  def index
+    
   end
   
   def new
@@ -16,6 +27,13 @@ class TopicPositionsController < ApplicationController
   def matches
     @topic_position = TopicPosition.find(params[:id])
     @matching = Debate.matching_debates(@topic_position)
+    
   end
+
+  ############ allow double rendering ###################
+  def reset_invocation_response
+    self.instance_variable_set(:@_response_body, nil)
+  end
+  #######################################################
 
 end
