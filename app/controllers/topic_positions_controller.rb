a579d7d334422619b1b075a949c0157a7c1e81db
Rick Batra <Batra.Rick@gmail.com>
2012-02-13 17:50:21 -0500

    Added list of suggested Topics.  Added javascript form validations to all forms

--- app/controllers/topic_positions_controller.rb
+++ app/controllers/topic_positions_controller.rb
@@ -9,7 +9,14 @@ class TopicPositionsController < ApplicationController
   end
   
   def matches
-    @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => params[:topic_position][:topic], :position => params[:topic_position][:position])    
+    params[:topic2] ? topic = params[:topic_position][:topic] + " vs " + params[:topic2] : topic = params[:topic_position][:topic]
+    if params[:topic_position][:position] == "vs" or  params[:topic_position][:position] == nil
+      position = nil
+    else
+      position = params[:topic_position][:position]
+    end
+    
+    @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => topic, :position => position)    
     @matching = Debate.matching_debates(@topic_position)
 	  
     respond_to do |format|
@@ -20,7 +27,7 @@ class TopicPositionsController < ApplicationController
   
   def index
     @matching = nil
-    @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => "...", :position => true)
+    @topic_position = TopicPosition.new(:debater_id => current_debater, :topic => "...", :position => nil)
     @trending = Suggested_Topic.trending(5)
   end
   
