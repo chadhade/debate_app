08ea2bc805f57761ef0635a4d6706a8e9f491d71
Rick Batra <Batra.Rick@gmail.com>
2012-02-14 17:09:41 -0500

    Added more javascript form validations

--- app/models/suggested_topic.rb
+++ app/models/suggested_topic.rb
@@ -1,5 +1,5 @@
 class Suggested_Topic < ActiveRecord::Base
-  attr_accessible #none
+  #attr_accessible #none
   
   def self.trending(number)
     return Suggested_Topic.find(:all, :limit => number, :order => "rating DESC")
