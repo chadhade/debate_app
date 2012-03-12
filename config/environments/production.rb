cb7e73823985bbba43440b9e39c8e2a0942dfdac
Rick Batra <Batra.Rick@gmail.com>
2012-03-11 20:31:06 -0400

    Modifed production.rb to configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,10 @@ DebateApp::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'debunky.com' }
+  
   # Enable threaded mode
   # config.threadsafe!
 
