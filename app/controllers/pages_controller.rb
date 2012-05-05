9c3a14b91771245c5b69df06d4e70efeeac3eb34
Rick Batra <Batra.Rick@gmail.com>
2012-05-04 21:54:16 -0400

    Added trending topics to the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :record_activity_time, :only => [:landing, :login]
   
   def landing
+    @trending = Suggested_Topic.trending(6)
   end
 
   def login
