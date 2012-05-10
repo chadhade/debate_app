b4b3948b39411d07a412a053b70aa2c7b8167cf0
Rick Batra <Batra.Rick@gmail.com>
2012-05-10 04:04:28 -0400

    Added auto-scrolling of trending topics on the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :record_activity_time, :only => [:landing, :login]
   
   def landing
-    @trending = Suggested_Topic.trending(6)
+    @trending = Suggested_Topic.trending(10)
   end
 
   def login
